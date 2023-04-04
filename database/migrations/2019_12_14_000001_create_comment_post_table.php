f98c34bd360438b3c73c36dedb06a1c02ec2f737
satriasuryap <115058337+satriasuryap@users.noreply.github.com>
2023-04-04 21:19:27 +0700

    migrate

--- database/migrations/2019_12_14_000001_create_comment_post_table.php
+++ database/migrations/2019_12_14_000001_create_comment_post_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::dropIfExists('personal_access_tokens');
+        
+        Schema::create('comment_post', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('comment');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_04_04_000000_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
+            $table->boolean('vote'); // 1 for upvote, 0 for downvote
+            $table->timestamps();
+
+            // create unique index on user_id and post_id
+            $table->unique(['user_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
