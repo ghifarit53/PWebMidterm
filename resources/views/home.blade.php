7e1bacdd429f0dd6eb3cf594aaad452185a8ea2a
Muhammad Dzaky Farhan <116267357+idzakyfarhan@users.noreply.github.com>
2023-04-05 23:13:06 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,12 @@
       <a href="/profile/{{ $post->user->username }} " class ="text-its-blue font-bold">{{ $post->user->username }}</a>
       <span class="text-gray-400"> on {{ $post->created_at->format('M d, Y') }} at {{ $post->created_at->format('h:i A') }}</span>
       <a href="/post/{{ $post->slug }}">
-        <h2 class="text-xl font-semibold mt-4">{{ $post->text }}</h2>
-      </a>
-
-      <a href=""></a>
+        <h2 class="text-xl font-semibold mt-2 mb-2">{{ $post->text }}</h2>
+          <a href="/post/{{ $post->slug }}" class="flex space-x-1 items-center">
+              <span class="material-icons" style="font-size:20px">comment</span>
+              <span>{{count($post->comments)}}</span>
+          </a>
+    </div>
     @endforeach
   @endif
 
