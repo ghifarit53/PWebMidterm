364f24a2d191f48e0a017ee5d24296f951642ab9
ghifarit53 <59758342+ghifarit53@users.noreply.github.com>
2023-04-05 11:13:47 +0700

    add icon for back button

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-    <a href="/">Back</a>
+    <div class="flex items-center">
+        <a class="text-its-blue flex items-center" href="/">
+            <span class="material-icons text-its-blue mr-1 flex-shrink-0" style="font-size: 18px">arrow_back_ios</span>
+            <span>Back</span>
+        </a>
+    </div>
 
     <div class="bg-gray-100 p-6 rounded-2xl mt-8"><p></p>
       <a href="/user/{{ $post->user->username }}" class="font-bold text-its-blue">{{ $post->user->username }}</a>
@@ -27,12 +32,12 @@
     </div>
     <div class="bg-gray-100 p-6 rounded-2xl mt-2">
         <form>
-            <label for="Comment" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Add your comment here</label>
+            <label for="Comment" class="mb-2 font-medium text-gray-900 sr-only dark:text-white">Add your comment here</label>
             <div class="relative">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" ></path></svg>
                 </div>
-                <input type="Comment" id="Comment" class="block w-full p-4 pl-5 text-sm focus:ring-blue-500 focus:border-blue-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-2xl" placeholder="Add your comment here!" maxlength="150" required>
+                <input type="Comment" id="Comment" class="block w-full p-4 pl-5 focus:ring-blue-500 focus:border-blue-500 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-2xl" placeholder="Add your comment here!" maxlength="150" required>
                 <button type="submit" class="text-white absolute right-2.5 bottom-11 bg-its-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 rounded-xl">Comment</button>
                 <div class="text-sm text-gray-500 mt-4"><span id="charCount">0</span>/150 characters used</div>
             </div>
